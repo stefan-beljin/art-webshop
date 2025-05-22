69e13f11da16551fa7202857a9b5e8745e3264d6
stefan-beljin <stefanbeljin782@gmail.com>
2025-05-22 20:47:31 +0200

    Add media allowed domains

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [{ protocol: "http", hostname: "local.art" }],
+    remotePatterns: [
+      { protocol: "http", hostname: "local.art" },
+      { protocol: "https", hostname: "ateljenatasabeljin.com" },
+      { protocol: "https", hostname: "*.ateljenatasabeljin.com" },
+    ],
   },
 };
 
