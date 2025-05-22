323c8fb9ba23807864034e0253f1389e329722c4
stefan-beljin <stefanbeljin782@gmail.com>
2025-05-22 22:19:54 +0200

    Add noindex rule

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,18 @@
 import { Montserrat_Alternates } from "next/font/google";
+import { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/organisms/Header";
 import Footer from "./components/organisms/Footer";
 
 const abril = Montserrat_Alternates({ weight: "500", subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Atelje Nataša Beljin",
+  description: "Atelje Nataša Beljin - Izrada i prodaja unikatnih slika",
+  robots: { index: process.env.ENVIRONMENT !== "PRODUCTION" ? false : true },
+};
+
+console.log("ENVIRONMENT", process.env.ENVIRONMENT);
 
 export default function RootLayout({
   children,
