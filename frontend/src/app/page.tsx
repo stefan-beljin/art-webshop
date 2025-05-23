f0a64dccd7e9eef724f08c2573f6208f3ab6b5e3
stefan-beljin <stefanbeljin782@gmail.com>
2025-05-24 00:51:48 +0200

    Add Form test component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@ import acfService from "./services/acfService";
 import Hero from "./components/organisms/Hero";
 import Gallery from "./components/organisms/Gallery";
 import ImageAndText from "./components/organisms/ImageAndText";
+import Form from "./components/organisms/Form";
 
 export default async function Home() {
   const data = await acfService.fetchPageAcfBySlug("homepage");
@@ -15,6 +16,7 @@ export default async function Home() {
       <Hero data={acf.banner} />
       <ImageAndText data={acf.imageAndText} />
       <Gallery data={acf.gallery} />
+      <Form />
     </div>
   );
 }
